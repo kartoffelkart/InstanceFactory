3106052c6ee3d5c84b60a206862b8e9eed65a954
Sonja <sonja_schaefer@gmx.de>
2016-12-03 09:29:48 +0100

    first Commit

---
+++ src/instancefactory/service/Tools.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instancefactory.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ *
+ * @author Sonja Schäfer <sonja_schaefer@gmx.de>
+ */
+public class Tools {
+
+    int getBudgetOfTheFirstNodes(ArrayList<Boolean> boughtList) {
+        int budget = 0;
+        return budget;
+    }
+
+    ArrayList<Integer> getRandomIntArray(int min, int max, Integer size) {
+        ArrayList<Integer> randomIntArrayList = new ArrayList<>();
+        int it = 0;
+        while (it < size) {
+
+            randomIntArrayList.add(ThreadLocalRandom.current().nextInt(min, max + 1));
+            it++;
+        }
+        return randomIntArrayList;
+    }
+}
