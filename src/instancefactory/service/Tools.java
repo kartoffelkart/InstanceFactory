75f41ec53a8717e337e914181a9dd15445ae03a4
Sonja <sonja_schaefer@gmx.de>
2017-02-09 11:19:01 +0100

    klappt mit abwechselnd union, left , right

--- src/instancefactory/service/Tools.java
+++ src/instancefactory/service/Tools.java
@@ -7,7 +7,7 @@ package instancefactory.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import  java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
