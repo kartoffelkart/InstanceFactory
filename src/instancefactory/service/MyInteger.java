a1219b7bff0dd124eac66a733b81605a0839696b
Sonja <sonja_schaefer@gmx.de>
2017-03-17 23:59:57 +0100

    Klasse MyInteger

---
+++ src/instancefactory/service/MyInteger.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instancefactory.service;
+
+/**
+ *
+ * @author Sonja Schäfer <sonja_schaefer@gmx.de>
+ */
+public class MyInteger {
+
+    /**
+     *
+     */
+    public Integer i;
+
+    public MyInteger(Integer i) {
+        this.i = i;
+    }
+
+}
